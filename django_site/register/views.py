945004f9c100dd2f78965d1c692e721099936405
Joner <joner.svensson@gmail.com>
2023-09-26 15:11:20 +0200

    Added "register" button

--- django_site/register/views.py
+++ django_site/register/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.messages import success
 
+def login(request):
+  # Login the user
+  ...
+
+  # Flash a message if login is successful
+  success(request, "You have successfully logged in!")
+
+  # Redirect the user to the home page
+  return redirect("/")
 # Create your views here.
 
 def register(repsonse):
